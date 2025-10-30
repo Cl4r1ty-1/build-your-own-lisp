5a1523ec1173deab17b2cb1020122054b5548528
Cl4r1ty-1 <136800640+Cl4r1ty-1@users.noreply.github.com>
2025-10-30 13:45:33 +0800

    finished 'reading expressions'

--- parsing.c
+++ parsing.c
@@ -59,12 +59,44 @@ lval* lval_sym(char* s) {
     return v;
 }
 
-lval* lvel_sexpr(void) {
+lval* lval_sexpr(void) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_SEXPR;
     v->count = 0;
     v->cell = NULL;
     return v;
+}
+
+lval* lval_add(lval* v, lval* x) {
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    v->cell[v->count-1] = x;
+}
+
+lval* lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ?
+        lval_num(x) : lval_err("Invalid number");
+}
+
+lval* lval_read(mpc_ast_t* t) {
+   
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+    
+    lval* x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strcmp(t->tag, "sexpr") == 0) { x = lval_sexpr(); }
+
+    for (int i = 0; i < t->children_num, i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
 }
 
 void lval_del(lval* v) {
